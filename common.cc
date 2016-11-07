ecc0edb08740698852146b050032f8053875b748
Carl Parker <carl94066@gmail.com>
2016-11-07 18:15:09 +0800

    Nothing

--- common.cc
+++ common.cc
@@ -5,8 +5,7 @@ namespace tdb {
 // HasPrefix returns whether a begins with b.
 bool HasPrefix(const string& a, const string& b) {
   return b.length() <= a.length() &&
-    equal(b.begin(), b.end(), a.begin());
+         equal(b.begin(), b.end(), a.begin());
 }
 
 }  // namespace tdb
-
